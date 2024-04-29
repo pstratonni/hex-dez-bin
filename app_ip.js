c11ec588b4bad871ed15b0e1ee1049adc5964ba8
pstratonni <pstratonni@gmail.com>
2024-04-29 16:31:21 +0200

    added all fuction

--- app_ip.js
+++ app_ip.js
@@ -42,13 +42,12 @@ const testUndefined = () => {
 const searchResultIP = () => {
   const [idxOctet, position] = searchOctet();
   const octetsValueBin = [];
+  let strValue = "";
   for (let i = 0; i < 4; i++) {
     if (i !== idxOctet) {
-      const octet = `<p class="render-octet">${octetsValue[i]}</p>`;
-      octetsValueBin.push(octet);
+      octetsValueBin.push(octetsValue[i]);
     } else {
       let bin = octetsValue[i];
-      let strValue = "";
       while (bin > 0) {
         strValue = (bin % 2) + strValue;
         bin = Math.trunc(bin / 2);
@@ -57,10 +56,7 @@ const searchResultIP = () => {
       for (let i = 0; i < 8 - length; i++) {
         strValue = 0 + strValue;
       }
-      octetsValueBin.push(
-        `<p class="render-octet-bin">${strValue.slice(0, position)}
-          <span class='pipe'>|</span>
-          ${strValue.slice(position)}</p>`
+      octetsValueBin.push(`${strValue.slice(0, position)}<span class='pipe'>|</span>${strValue.slice(position)}`
       );
     }
   }
@@ -69,6 +65,17 @@ const searchResultIP = () => {
     "<p class='point'>/</p>" +
     prefixValue;
   searchMask(idxOctet, position);
+  const netz = searchNetz(idxOctet, position, strValue)
+  if (+prefixValue<32){
+    searchFirstHost(netz)
+    const latestHost = searchLatestHost(idxOctet, position, strValue)
+    searchBroadcast(latestHost)
+  }else{
+    searchBroadcast()
+    document.querySelector(".render-first_dec").innerHTML = document.querySelector(".render-netz_dec").innerHTML
+    document.querySelector(".render-latest_bin").innerHTML = document.querySelector(".render-netz_bin").innerHTML
+  document.querySelector(".render-latest_dec").innerHTML = document.querySelector(".render-netz_dec").innerHTML
+  }
 };
 
 const searchOctet = () => {
@@ -94,21 +101,123 @@ const searchMask = (idxOctet, position) => {
       maskBin.push("255");
     } else if (i === idxOctet) {
       for (let i = 1; i < 9; i++) {
-        if (i <= position) {
+        if (i < position) {
           binOctet = binOctet + 1;
           decOctet = decOctet + Math.pow(2, 8 - i);
-        } else {
+        } else if (i==position){
+          decOctet = decOctet + Math.pow(2, 8 - i);
+          binOctet = binOctet + 1 + "<span class='pipe'>|</span>";
+        }
+        else {
           binOctet = binOctet + "0";
         }
       }
-      mask.push(str(decOctet))
+      mask.push(decOctet.toString())
       maskBin.push(binOctet);
     } else {
       mask.push("0");
       maskBin.push("0");
     }
   }
+  document.querySelector(".render-mask_bin").innerHTML = maskBin.join("<p class='point'>.</p>") 
+  document.querySelector(".render-mask_dec").innerHTML = mask.join("<p class='point'>.</p>") 
 };
+
+const searchNetz = (idx, position, octet) =>{
+  const netz = []
+  const netzBin = []
+  let netzOctet =""
+  for (let i=0; i<4; i++){
+    if (i<idx){
+      netz.push(octetsValue[i])
+      netzBin.push(octetsValue[i])
+    } else if (i===idx){
+      const octetDec = octet.slice(0,position).split("").reduce((accu, currentValue, idx)=>{
+      accu = accu + (+currentValue)*Math.pow(2,7-idx)
+      return accu
+      },0) 
+      netz.push(octetDec)
+      let zeros = ""
+      for (let j=position; j<8; j++){
+        zeros = zeros + "0"
+      }
+      netzOctet = octet.slice(0,position) + "<span class='pipe'>|</span>" + zeros;
+      netzBin.push(netzOctet)
+    }
+    else{
+      netz.push("0")
+      netzBin.push("0")
+    }
+  }
+  document.querySelector(".render-netz_bin").innerHTML = netzBin.join("<p class='point'>.</p>") +
+  "<p class='point'>/</p>" +
+  prefixValue;
+  document.querySelector(".render-netz_dec").innerHTML = netz.join("<p class='point'>.</p>") +
+  "<p class='point'>/</p>" +
+  prefixValue;
+  return netz
+}
+
+const searchFirstHost = (netz) => {
+netz[netz.length-1] = +netz[netz.length-1] + 1 
+document.querySelector(".render-first_dec").innerHTML = netz.join("<p class='point'>.</p>") +
+  "<p class='point'>/</p>" +
+  prefixValue;
+}
+
+const searchLatestHost = (idxOctet, position, strValue) => {
+  const latestHostBin =[]
+  const latestHost = []
+  for (let i=0; i<4; i++){
+    if (i<idxOctet){
+      latestHost.push(octetsValue[i])
+      latestHostBin.push(octetsValue[i])
+    }else if(i===idxOctet){
+      let octet = strValue.slice(0, position)
+      let binOctet = strValue.slice(0, position)
+      let one = ""
+      for (let j=position; j<8; j++){
+        if (i===3 && j==7){
+          one = one + "0"
+          break
+        }
+        one = one + "1"
+      }
+      octet = octet + one
+      binOctet = binOctet + "<span class='pipe'>|</span>" + one
+      const octetDec = octet.split("").reduce((accu, currentValue, idx)=>{
+        accu = accu + (+currentValue)*Math.pow(2,7-idx)
+        return accu
+        },0) 
+        latestHost.push(octetDec)
+        latestHostBin.push(binOctet)
+    }
+    else if (i===3){
+      latestHost.push("254")
+      latestHostBin.push("254")
+    }
+    else{
+      latestHost.push("255")
+      latestHostBin.push("255")
+    }
+  }
+  document.querySelector(".render-latest_bin").innerHTML = latestHostBin.join("<p class='point'>.</p>") +
+  "<p class='point'>/</p>" +
+  prefixValue;
+  document.querySelector(".render-latest_dec").innerHTML = latestHost.join("<p class='point'>.</p>") +
+  "<p class='point'>/</p>" +
+  prefixValue;
+  return latestHost
+}
+
+const searchBroadcast = (broadcast=octetsValue) => {
+  if(+prefixValue<32){
+    broadcast[broadcast.length-1] = +broadcast[broadcast.length-1] + 1
+  }
+  document.querySelector(".render-broadcast_dec").innerHTML = broadcast.join("<p class='point'>.</p>") +
+  "<p class='point'>/</p>" +
+  prefixValue;
+}
 
 for (let octet of octets) {
   octet.addEventListener("input", addIPOctet);
